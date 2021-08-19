13601185b7180baeea051a83c39a03e0fe8aa17b
Allan Queiroz <56939211+allanadfq@users.noreply.github.com>
2021-08-18 22:12:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "@fontsource/roboto";
+import "./style.css";
+
+import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <h1>BILLS-NOT-THE-GATE</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<DeleteIcon />}
+        fullWidth={true}
+      >
+        Hello World
+      </Button>
     </div>
   );
 }
