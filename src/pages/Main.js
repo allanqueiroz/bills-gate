783cb3d48c99a6cb64440c1852012d7c3f7d5da0
Allan Queiroz <56939211+allanadfq@users.noreply.github.com>
2021-08-31 22:56:32 -0300

    off

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -25,17 +25,6 @@ const styles = makeStyles({
     padding: "0 10px",
   },
 });
-//styled component api
-const MyTitleH2 = styled(Button)({
-  background: "linear-gradient(45deg, #FF8E53 40%, #FE6B8B 90%)",
-  border: 0,
-  borderRadius: 3,
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  color: "white",
-  height: 48,
-  padding: "0 30px",
-  display: "block",
-});
 
 const Main = () => {
   const classes = styles();
